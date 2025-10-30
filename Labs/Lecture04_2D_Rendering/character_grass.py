64ee082fbea080847f345feca7b116b7358743d0
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-10-30 15:41:34 +0900

    .

--- Labs/Lecture04_2D_Rendering/character_grass.py
+++ Labs/Lecture04_2D_Rendering/character_grass.py
@@ -2,6 +2,40 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 400
+while (x < 780):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, 90)
+    x = x + 5
+    delay(0.01)
+
+y = 90
+while (y < 560) :
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(780, y)
+    y = y + 5
+    delay(0.01)
+
+while (x > 10) :
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, 560)
+    x = x - 5
+    delay(0.01)
+
+while (y > 90) :
+     clear_canvas_now()
+     grass.draw_now(400, 30)
+     character.draw_now(10, y)
+     y = y - 5
+     delay(0.01)
+
 
 close_canvas()
+
+
